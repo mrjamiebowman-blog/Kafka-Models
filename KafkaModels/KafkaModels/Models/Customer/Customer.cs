7a1442dce16a7c6f518e2280f5fc55870be100b2
Jamie Bowman <mrjamiebowman@gmail.com>
2020-06-14 20:34:21 -0400

    commit

---
+++ KafkaModels/KafkaModels/Models/Customer/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace KafkaModels.Models.Customer
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Kafka Models Git Submodules Models
+This is part of a blog post and belongs to the main repository [Kafka-Models-Git-Submodules-Integrations](https://github.com/mrjamiebowman-blog/Kafka-Models-Git-Submodules-Integrations)
+
+
