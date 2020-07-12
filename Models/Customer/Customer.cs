a76814eaf8cd323292bc2282fad71cd4159e4db2
Jamie Bowman <mrjamiebowman@gmail.com>
2020-07-11 20:19:34 -0400

    updates with data annotations.

--- Models/Customer/Customer.cs
+++ Models/Customer/Customer.cs
@@ -1,16 +1,29 @@
-﻿namespace KafkaModels.Models.Customer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KafkaModels.Models.Customer
 {
     public class Customer
     {
         public int? CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        
+
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [Display(Name = "Billing Address")]
         public Address BillingAddress { get; set; }
 
+        [Required]
+        [Display(Name = "Shipping Address")]
         public Address ShippingAddress { get; set; }
     }
 }
